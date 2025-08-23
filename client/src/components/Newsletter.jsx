6dc4f16274e88e8f107d1d79465e6e2968b9c526
Vansh150705 <vansh150705@gmail.com>
2025-08-23 13:55:45 +0530

    Color Changed

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -7,7 +7,7 @@ const Newsletter = () => {
       <p className='md:text-lg text-gray-500/70 pb-8'>Subscribe to get the latest blog, new tech, and exclusive news.</p>
       <form className='flex items-center justify-between max-w-2xl w-full md:h-13 h-12'>
         <input className='border border-gray-300 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500' type="text" placeholder='Enter your email id' required/>
-        <button type='submit' className='md:px-12 px-8 h-full text-white bg-[#1ABC9C] hover:bg-green-900 transition-all cursor-pointer rounded-md rounded-l-none'>Subscribe</button>
+        <button type='submit' className='md:px-12 px-8 h-full text-white bg-[#1ABC9C] hover:bg-green-800 transition-all cursor-pointer rounded-md rounded-l-none'>Subscribe</button>
       </form>
     </div>
   )
