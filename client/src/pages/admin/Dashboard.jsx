2775db9cdb6593b75dddacab7de6fdc7bd6dbb76
Vansh150705 <vansh150705@gmail.com>
2026-05-05 21:45:00 +0530

    a11y(client): add alt text to dashboard stat icons

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -46,7 +46,7 @@ const Dashboard = () => {
         <div className='flex flex-wrap gap-4'>
 
             <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
-                <img src={assets.dashboard_icon_1} alt="" />
+                <img src={assets.dashboard_icon_1} alt="Blogs" />
                 <div>
                     <p className='text-xl font-semibold text-gray-600'>{dashboardData.blogs}</p>
                     <p className='text-gray-400 font-light'>Blogs</p>
@@ -54,7 +54,7 @@ const Dashboard = () => {
             </div>
 
             <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
-                <img src={assets.dashboard_icon_2} alt="" />
+                <img src={assets.dashboard_icon_2} alt="Comments" />
                 <div>
                     <p className='text-xl font-semibold text-gray-600'>{dashboardData.comments}</p>
                     <p className='text-gray-400 font-light'>Comments</p>
@@ -62,7 +62,7 @@ const Dashboard = () => {
             </div>
 
             <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
-                <img src={assets.dashboard_icon_3} alt="" />
+                <img src={assets.dashboard_icon_3} alt="Drafts" />
                 <div>
                     <p className='text-xl font-semibold text-gray-600'>{dashboardData.drafts}</p>
                     <p className='text-gray-400 font-light'>Drafts</p>
@@ -80,7 +80,7 @@ const Dashboard = () => {
 
         <div>
             <div className='flex items-center gap-3 m-4 mt-6 text-gray-600'>
-                <img src={assets.dashboard_icon_4} alt="" />
+                <img src={assets.dashboard_icon_4} alt="Latest blogs" />
                 <p>Latest Blogs</p>
             </div>
 
