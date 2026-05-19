73a2e403ae7c7ca491b448fd47b938c1c511f1bc
Vansh150705 <vansh150705@gmail.com>
2026-05-19 22:05:00 +0530

    a11y(client): add descriptive alt text on the blog page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -81,7 +81,7 @@ const Blog = () => {
       </div>
 
       <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
-          <img src={data.image} alt="" className='rounded-3xl mb-5'/>
+          <img src={data.image} alt={data.title} className='rounded-3xl mb-5'/>
 
           <div className='rich-text max-w-3xl mx-auto' dangerouslySetInnerHTML={{__html: data.description}}></div>
 
@@ -92,7 +92,7 @@ const Blog = () => {
                 {comments.map((item, index)=>(
                   <div key={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600'>
                     <div className='flex items-center gap-2 mb-2'>
-                      <img src={assets.user_icon} alt="" className='w-6'/>
+                      <img src={assets.user_icon} alt={`${item.name} avatar`} className='w-6'/>
                       <p className='font-medium'>{item.name}</p>
                     </div>
                     <p className='text-sm max-w-md ml-8'>{item.content}</p>
@@ -119,9 +119,9 @@ const Blog = () => {
           <div className='my-24 max-w-3xl mx-auto'>
               <p className='font-semibold my-4'>Share this article on social media</p>
               <div className='flex'>
-                <img src={assets.facebook_icon} width={50} alt="" />
-                <img src={assets.twitter_icon} width={50} alt="" />
-                <img src={assets.googleplus_icon} width={50} alt="" />
+                <img src={assets.facebook_icon} width={50} alt="Share on Facebook" className='cursor-pointer' />
+                <img src={assets.twitter_icon} width={50} alt="Share on Twitter" className='cursor-pointer' />
+                <img src={assets.googleplus_icon} width={50} alt="Share on Google Plus" className='cursor-pointer' />
               </div>
           </div>
       </div>
