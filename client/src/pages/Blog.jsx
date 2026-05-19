347a96698f678962641cc6fe4afc85cec548160c
Vansh150705 <vansh150705@gmail.com>
2026-05-19 10:15:00 +0530

    feat(client): show reading time on the blog page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -6,6 +6,7 @@ import Moment from 'moment'
 import Footer from '../components/Footer'
 import Loader from '../components/Loader'
 import { useAppContext } from '../context/AppContext'
+import { getReadingTime } from '../utils/readingTime'
 import toast from 'react-hot-toast'
 
 const Blog = () => {
@@ -73,6 +74,7 @@ const Blog = () => {
         <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
         <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
         <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-[#1ABC9C]'>Vansh Mahajan</p>
+        <p className='text-sm text-gray-500'>{getReadingTime(data.description)} min read</p>
       </div>
 
       <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
